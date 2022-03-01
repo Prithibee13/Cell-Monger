b369f9c61a328a27debadc36f1a3246f90b59942
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-03-01 12:14:53 +0600

    dynamic cards

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 
 
-const cardMaker = (img, titles) =>
+const cardMaker = (brand, img, titles) =>
 {
     let card = 
     `
     <div class="col">
-        <div class="card">
-            <img src=${img} class="card-img-top" alt="...">
+        <div class="card h-100">
+            <img src='${img}' class="card-img-top w-50 h-50" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${titles}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h3 class="card-title">${titles}</h3>
+                <h4 class="card-text">${brand}</h4>
                 </div>
             </div>
         </div>
     
-    `;
+    `
+    ;
 
     return card;
 }
@@ -27,6 +28,34 @@ const apiFetching =  () =>
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => displayData(data));
+}
+
+
+const displayData = (mobileDetails) =>
+{
+    const dataArray = mobileDetails.data;
+
+    const cardDiv = document.getElementById("cardDiv");
+
+    let phoneBrand;
+    let phoneName; 
+    let phoneImg;
+
+    for(data of dataArray) 
+    {
+        //console.log(data);
+        phoneBrand = data.brand;
+        phoneImg = data.image;
+        phoneName = data.phone_name;
+
+        const gridContainer  = document.createElement("div");
+        gridContainer.innerHTML = `
+            ${cardMaker(phoneBrand,phoneImg , phoneName)}
+        `
+        cardDiv.append(gridContainer);
+        console.log(cardDiv)
+
+    };
 }
 
